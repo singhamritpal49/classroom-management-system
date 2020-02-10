93e86ad81f1100c78a242670bbdc432f23f30d06
Amritpal Singh <singhamritpal49@gmail.com>
2020-02-10 15:01:13 -0500

    added all API endpoints

--- index.js
+++ index.js
@@ -3,26 +3,38 @@ const roomData = require('./Api/1_roomData.json')
 const deskData = require('./Api/2_deskData.json')
 const studentData = require('./Api/3_studentData.json')
 const dailyData = require('./Api/4_dailyData.json')
-
-
-
-
-
-
 const app = express()
 
-app.get('/api/students', (req, res) => {
+
+
+
+
+
+app.get('/classroom/students', (req, res) => {
     if (!studentData) {
         res.status(404)
     }
     res.status(200).send(studentData)
 })
 
-app.get('/api/rooms', (req, res) => {
+app.get('/classroom/rooms', (req, res) => {
     if (!roomData) {
         res.status(404)
     }
     res.status(200).send(roomData)
+})
+
+app.get('/classroom/deskdata', (req,res) => {
+    if (!deskData) {
+        res.status(400)
+    }
+    res.status(200).send(deskData)
+})
+app.get('/classroom/dailydata', (req,res) => {
+    if (!dailyData) {
+        res.status(400)
+    }
+    res.status(200).send(dailyData)
 })
 
 
