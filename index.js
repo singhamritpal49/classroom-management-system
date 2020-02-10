afc07fb37cd9d02dd178dc885f57fb67080b3476
Amritpal Singh <singhamritpal49@gmail.com>
2020-02-10 14:51:14 -0500

    added students data API endpoint

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 const express = require('express');
-const router = express.Router();
+const roomData = require('./Api/1_roomData.json')
+const deskData = require('./Api/2_deskData.json')
+const studentData = require('./Api/3_studentData.json')
+const dailyData = require('./Api/4_dailyData.json')
 
 
-router.get()
+
+
+
+
+const app = express()
+
+app.get('/api/students', (req, res) => {
+    if (!studentData) {
+        res.status(404)
+    }
+    res.status(200).send(studentData)
+})
+
+// app.get('/api/rooms', (req, res) => {
+//     if (!roomData) {
+//         res.status(404)
+//     }
+//     res.status(200).send(roomData)
+// })
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Running Successfully at 3000 Port")
+})
+
+
